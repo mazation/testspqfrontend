282384496626cecc371040b51f52b9f23c3d3300
mazation <mazation96@gmail.com>
2020-10-05 04:04:48 +0500

    added calls

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import config from './config'
 import store from './store'
+import Paginate from 'vuejs-paginate'
 
 new Vue({
   router,
   store,
+  config,
   render: h => h(App),
 }).$mount('#app')
+
+Vue.component('paginate', Paginate)
